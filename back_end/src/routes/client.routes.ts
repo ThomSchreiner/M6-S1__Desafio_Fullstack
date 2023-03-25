34084581cf993575f5eb120b712fad5af67d5ac4
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-25 12:48:35 -0300

    feat: add login route and jwt authorization

--- back_end/src/routes/client.routes.ts
+++ back_end/src/routes/client.routes.ts
@@ -7,11 +7,12 @@ import {
     updateClientController,
 } from "../controllers/client.controllers";
 import { ensureClientExistsMiddleware } from "../middlewares/ensureClientExists.middleware";
+import { verifyTokenMiddleware } from "../middlewares/verifyToken.middleware";
 
 export const clientRoutes = Router();
 
 clientRoutes.get("", listClientsController);
 clientRoutes.post("", createClientController);
-clientRoutes.get("/:id", ensureClientExistsMiddleware, retrieveClientController);
-clientRoutes.patch("/:id", ensureClientExistsMiddleware, updateClientController);
-clientRoutes.delete("/:id", ensureClientExistsMiddleware, destroyClientController);
+clientRoutes.get("/:id", verifyTokenMiddleware, ensureClientExistsMiddleware, retrieveClientController);
+clientRoutes.patch("/:id", verifyTokenMiddleware, ensureClientExistsMiddleware, updateClientController);
+clientRoutes.delete("/:id", verifyTokenMiddleware, ensureClientExistsMiddleware, destroyClientController);

66aa0c6c7d56c31e4c4cab27ccc31fdee93ea3be
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-26 11:18:47 -0300

    feat: add request typing of contact

--- back_end/src/@types/express/index.d.ts
+++ back_end/src/@types/express/index.d.ts
@@ -1,9 +1,11 @@
 import { Client } from "../../entities/client.entity";
+import { Contact } from "../../entities/contact.entity";
 
 declare global {
     namespace Express {
         interface Request {
             paramClient: Client;
+            paramContact: Contact;
             authClient: Client;
         }
     }

6238b075783ae4a9fe8c23480c867c030c4dbf50
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-27 20:19:28 -0300

    feat: add typing of components

--- front_end/src/interfaces/pages.interfaces.ts
+++ front_end/src/interfaces/pages.interfaces.ts
@@ -1,9 +1,20 @@
-import { iContact } from "./user.interfaces";
+import { iClient, iContact } from "./user.interfaces";
 
 export interface iDashboardProps {
     contacts: iContact[];
+    client: iClient;
 }
 
 export interface iStyledHeader {
     userName: string;
+}
+
+export interface iStyledLink {
+    href: string;
+    text: string;
+}
+
+export interface iModalCreateContatc {
+    isOpen: boolean;
+    onClose: () => void;
 }

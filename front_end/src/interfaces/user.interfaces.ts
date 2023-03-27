86d0139b0f4802832396cefca58bd7578211e881
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-27 17:00:00 -0300

    feat: add register page

--- front_end/src/interfaces/user.interfaces.ts
+++ front_end/src/interfaces/user.interfaces.ts
@@ -1,3 +1,11 @@
+export interface iClientRegister {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    phone_number: string;
+}
+
 export interface iClient {
     id: string;
     first_name: string;

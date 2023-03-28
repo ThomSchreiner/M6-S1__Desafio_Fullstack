6b5b8acf259ed65d12938ea12e06ed45b0909fc6
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-28 13:21:02 -0300

    feat: add modal to edit contact

--- front_end/src/schemas/contact.schemas.ts
+++ front_end/src/schemas/contact.schemas.ts
@@ -3,3 +3,5 @@ import { iContactRegister } from "@/interfaces/user.interfaces";
 import { clientRegisterSchema } from "./client.schemas";
 
 export const contactRegisterSchema: ObjectSchema<iContactRegister> = clientRegisterSchema.omit(["password"]);
+
+export const contactUpdateSchema = contactRegisterSchema.partial();

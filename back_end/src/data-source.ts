ec196a512fc9f4df671a14d34f26e3e3ab8aa18d
Thomas Schreiner <iteano.thom@hotmail.com>
2023-04-03 15:34:50 -0300

    feat: add configs to use database production

--- back_end/src/data-source.ts
+++ back_end/src/data-source.ts
@@ -1,16 +1,33 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import path from "path";
 import "dotenv/config";
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    logging: true,
-    synchronize: false,
-    entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-    migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
-});
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath = path.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath = path.join(__dirname, "./migrations/**.{js,ts}");
+    const nodeEnv = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
+    }
+
+    return {
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        logging: true,
+        synchronize: false,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+};
+
+export const AppDataSource = new DataSource(setDataSourceConfig());

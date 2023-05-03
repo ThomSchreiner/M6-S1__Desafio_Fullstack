11c10687e1efa1d4385242dc0b394d903133f941
Thomas Schreiner <iteano.thom@hotmail.com>
2023-05-03 19:48:29 -0300

    feat: change database connect method

--- back_end/src/data-source.ts
+++ back_end/src/data-source.ts
@@ -18,11 +18,7 @@ const setDataSourceConfig = (): DataSourceOptions => {
 
     return {
         type: "postgres",
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        url: process.env.DATABASE_URL,
         logging: true,
         synchronize: false,
         entities: [entitiesPath],
